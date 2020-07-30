258ba9a0e13fc66a2cf1621b8c3872282b72dad5
mxbialostocki <matt.bialostocki@gmail.com>
2020-07-31 11:07:56 +1200

    stripped phoGal component

---
+++ src/components/photographyGallery.js
@@ -0,0 +1,29 @@
+import React from "react"
+// import PropTypes from "prop-types"
+// import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.css"
+
+const PhotographyGallery = () => {
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
+
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  )
+}
+
+// PhotographyGallery.propTypes = {
+//   children: PropTypes.node.isRequired,
+// }
+
+export default PhotographyGallery
