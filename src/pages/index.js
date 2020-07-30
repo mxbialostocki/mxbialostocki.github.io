7f21fef4df8125b20d42ee382154953a178ee48e
mxbialostocki <matt.bialostocki@gmail.com>
2020-07-30 16:11:00 +1200

    link-splitting component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 import { Grid, Typography } from '@material-ui/core'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Link from "../components/link"
 
 const IndexPage = () => (
   <Layout>
