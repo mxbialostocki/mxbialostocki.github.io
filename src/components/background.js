f138fd5b07ef9cc109e4f9d402e0f6fb15b6822c
mxbialostocki <matt.bialostocki@gmail.com>
2020-07-31 14:51:12 +1200

    touch background component

---
+++ src/components/background.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+import styled from 'styled-components'
+
+import BackgroundImage from 'gatsby-background-image'
+
+const Background = ({ className }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "background-moon.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      // Set ImageData.
+      const imageData = data.desktop.childImageSharp.fluid
+      return (
+        <BackgroundImage
+          Tag="section"
+          className={className}
+          fluid={imageData}
+          backgroundColor={`#040e18`}
+        >
+          <div style={{ width: '100vw', height: '100vh' }}></div>
+        </BackgroundImage>
+      )
+    }}
+  />
+)
+
+const StyledBackground = styled(Background)`
+  width: 100%;
+  background-position: bottom center;
+  background-repeat: repeat-y;
+  background-size: cover;
+`
+
+export default StyledBackground
