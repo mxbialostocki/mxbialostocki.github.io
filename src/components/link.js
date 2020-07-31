ad5fd43b1a9a6cf96270f1ff4f2850e95784b937
mxbialostocki <matt.bialostocki@gmail.com>
2020-07-31 14:50:44 +1200

    tidy link

--- src/components/link.js
+++ src/components/link.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import { Link as GatsbyLink } from "gatsby"
 
-// Since DOM elements <a> cannot receive activeClassName
-// and partiallyActive, destructure the prop here and
-// pass it only to GatsbyLink
+// Since DOM elements <a> cannot receive activeClassName and partiallyActive, destructure the prop here and pass it only to GatsbyLink
 const Link = ({ children, to, activeClassName, partiallyActive, ...other }) => {
-  // Tailor the following test to your environment.
-  // This example assumes that any internal link (intended for Gatsby)
-  // will start with exactly one slash, and that anything else is external.
+  // This test assumes that any internal link (intended for Gatsby) will start with exactly one slash, and that anything else is external.
   const internal = /^\/(?!\/)/.test(to)
 
   // Use Gatsby Link for internal links, and <a> for others
