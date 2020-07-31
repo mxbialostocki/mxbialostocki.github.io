3c3450246b4f5632f5967a73226762d91d7bafb7
mxbialostocki <matt.bialostocki@gmail.com>
2020-07-31 12:55:00 +1200

    basic photography component written

--- src/components/photographyGallery.js
+++ src/components/photographyGallery.js
@@ -1,24 +1,56 @@
 import React from "react"
 // import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { Grid, Container, Typography } from '@material-ui/core'
 
-import Header from "./header"
-import "./layout.css"
-
-const PhotographyGallery = () => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-
+export function PhotographyGallery () {
+  const data = useStaticQuery(graphql`
+    query {
+      portrait: file(relativePath: { eq: "mxbialostocki191126-sgts04-1129.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 900, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      craft: file(relativePath: { eq: "mxbialostocki191126-sgts04-1270.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 900, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      event: file(relativePath: { eq: "mxbialostocki191126-sgts04-1891.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 900, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
-    <React.Fragment>
-    </React.Fragment>
+  <React.Fragment>
+    <Grid container direction="row" sm={12} spacing={1} justify="space-between">
+      <Grid container item sm={6} spacing={1} direction="column">
+        <Grid item>
+          <Img fluid={data.portrait.childImageSharp.fluid} />
+          <Typography>portrait</Typography>
+        </Grid>
+      </Grid>      
+      <Grid container item sm={6} spacing={1} direction="column" justify="space-between">
+        <Grid item>
+          <Img fluid={data.craft.childImageSharp.fluid} />
+          <Typography>craft</Typography>
+        </Grid>
+        <Grid item>
+          <Img fluid={data.event.childImageSharp.fluid} />
+          <Typography>event</Typography>
+        </Grid>        
+      </Grid>
+    </Grid>            
+  </React.Fragment>  
   )
 }
 
--- src/pages/photography.js
+++ src/pages/photography.js
@@ -4,17 +4,22 @@ import { Grid, Typography } from '@material-ui/core'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PhotographyGallery from "../components/photographyGallery"
 
 const Photography = () => (
   <Layout>
     <SEO title="Photography" />
     <Grid container direction="column">
-      <Typography variant="h4">
-        Some things I say about photography: Look at all these things I say
-      </Typography>
+      <Grid item>
+        <Typography variant="h4">
+          Some things I say about photography: Look at all these things I say
+        </Typography>
+      </Grid>
+    </Grid>
+    <PhotographyGallery />
+    <Grid item>
       <Link to="/">go home</Link>
     </Grid>
-    
   </Layout>
 )
 
