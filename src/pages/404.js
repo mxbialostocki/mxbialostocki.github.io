e478c986ae3aa43121e49d1579181c851a9746bb
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-05 14:57:38 +1200

    align 404 for common use

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Typography } from '@material-ui/core'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,8 +7,9 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Typography variant='h1'>NOT FOUND</Typography>
+    <Typography variant='body1'>Existence is futile, the page you're lookin' for doesn't persist</Typography>
+    <Typography variant='body1'>... but check back soon because it probably will in a few days :O</Typography>
   </Layout>
 )
 
