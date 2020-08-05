400e1895499f4dec3bdc10b5d8421557be0c4dde
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-06 11:37:30 +1200

    touch up photography structure

--- src/components/photographyGallery.js
+++ src/components/photographyGallery.js
@@ -1,10 +1,9 @@
 import React from "react"
-// import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import { Grid, Typography } from '@material-ui/core'
 
-export function PhotographyGallery () {
+const PhotographyGallery = () => {
   const data = useStaticQuery(graphql`
     query {
       portrait: file(relativePath: { eq: "mxbialostocki191126-sgts04-1129.jpg" }) {
@@ -31,29 +30,29 @@ export function PhotographyGallery () {
     }
   `)
   return (
-  <React.Fragment>
-    <Grid container direction="row" spacing={1} justify="space-between">
-      <Grid container item sm={6} spacing={1} direction="column">
-        <Grid item>
-          <Img fluid={data.portrait.childImageSharp.fluid} />
-          <Typography>portrait</Typography>
-        </Grid>
+    <React.Fragment>
+      <Grid container direction='row' spacing={4} >
+        
+          <Grid item lg={3} md={6} xs={12}>
+            <Img fluid={data.portrait.childImageSharp.fluid} />
+            <Typography variant="h5">portrait</Typography>
+          </Grid>
+          <Grid item lg={3} md={6} xs={12}>
+            <Img fluid={data.event.childImageSharp.fluid} />
+            <Typography variant="h5">event</Typography>
+          </Grid>
+          <Grid item lg={3} md={6} xs={12}>
+            <Img fluid={data.craft.childImageSharp.fluid} />
+            <Typography variant="h5">craft</Typography>
+          </Grid>
+          <Grid item lg={3} md={6} xs={12}>
+            <Img fluid={data.event.childImageSharp.fluid} />
+            <Typography variant="h5">event</Typography>
+          </Grid>
+
       </Grid>      
-      <Grid container item sm={6} spacing={1} direction="column" justify="space-evenly">
-        <Grid item>
-          <Img fluid={data.craft.childImageSharp.fluid} />
-          <Typography>craft</Typography>
-          <Img fluid={data.event.childImageSharp.fluid} />
-          <Typography>event</Typography>
-        </Grid>        
-      </Grid>
-    </Grid>            
-  </React.Fragment>  
+    </React.Fragment>
   )
 }
-
-// PhotographyGallery.propTypes = {
-//   children: PropTypes.node.isRequired,
-// }
 
 export default PhotographyGallery
--- src/pages/photography.js
+++ src/pages/photography.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import { Grid, Typography } from '@material-ui/core'
 
 import Layout from "../components/layout"
@@ -7,20 +6,29 @@ import SEO from "../components/seo"
 import PhotographyGallery from "../components/photographyGallery"
 
 const Photography = () => (
-  <Layout>
-    <SEO title="Photography" />
-    <Grid container direction="column">
-      <Grid item>
-        <Typography variant="h4">
-          Some things I say about photography: Look at all these things I say
-        </Typography>
+  <React.Fragment>
+    <Layout>
+      <SEO title="Photography" />
+      <Grid container direction="column" spacing={4}>
+
+        <Grid item>
+          <Typography variant="h2">photography</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h5">
+            Some things I say about photography: Look at all these things I say
+          </Typography>
+        </Grid>
+
       </Grid>
-    </Grid>
-    <PhotographyGallery />
-    <Grid item>
-      <Link to="/">go home</Link>
-    </Grid>
-  </Layout>
+      <PhotographyGallery />
+      <Grid container direction='row' spacing={4} >
+        <Grid item lg={12}>
+          <Typography variant='h2'>`rate card`</Typography>
+        </Grid>
+      </Grid>
+    </Layout>
+  </React.Fragment>
 )
 
 export default Photography
