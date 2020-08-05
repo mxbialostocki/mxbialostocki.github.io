6dab361afd84466805156ee9c92a685578649b13
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-05 16:14:08 +1200

    create custom theme framework

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,14 @@ module.exports = {
     author: `@mxbialostocki`,
   },
   plugins: [
-    `gatsby-plugin-material-ui`,
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -27,7 +34,8 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
+    },    
+    `custom-mui-theme`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ plugins/custom-mui-theme/theme/index.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = {
+  primary: { main: '#212121' },
+  secondary: { main: '#D81B60' }
+};
+const themeName = 'Mine Shaft Amaranth Bluebird';
+
+export default createMuiTheme({ palette, themeName });
