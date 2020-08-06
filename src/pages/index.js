ae74689f54b5ed26b4112ca3ce5826c53ef17229
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-07 10:28:22 +1200

    new styling to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { Grid, Typography } from '@material-ui/core'
 
+import useStyles from '../../plugins/custom-mui-theme/theme/custom'
+
 import Layout from "../components/layout"
 import Pages from "../components/pages"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
+  const { primary } = useStyles()
+  
   return (
     <React.Fragment>
       <Layout>
@@ -17,7 +21,7 @@ const IndexPage = () => {
           </Grid>        
           <Grid item>
             <Typography variant="h5">My name is Bam.</Typography>
-            <Typography variant="h5">I am the dev you're looking for.</Typography>
+            <Typography variant="h5" className={primary}>I am the dev you're looking for.</Typography>
           </Grid>
 
         </Grid>
