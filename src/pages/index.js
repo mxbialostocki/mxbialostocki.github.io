d639415c1fa3a5350d3751556289aa90027c5fe6
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-07 10:25:54 +1200

    convert index to fragment structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,26 +5,26 @@ import Layout from "../components/layout"
 import Pages from "../components/pages"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <React.Fragment>
-    <Layout>
-      <SEO title="Home" />
-      <Grid container direction="column" spacing={4}>
+const IndexPage = () => {
+  return (
+    <React.Fragment>
+      <Layout>
+        <SEO title="Home" />
+        <Grid container direction="column" spacing={4}>
 
-        <Grid item>
-          <Typography variant="h2">hello, Creature</Typography>
-        </Grid>        
-        <Grid item>
-          <Typography variant="h5">My name is Bam.</Typography>
-          <Typography variant="h5">I am the dev you're looking for.</Typography>
+          <Grid item>
+            <Typography variant="h2">hello, Creature</Typography>
+          </Grid>        
+          <Grid item>
+            <Typography variant="h5">My name is Bam.</Typography>
+            <Typography variant="h5">I am the dev you're looking for.</Typography>
+          </Grid>
+
         </Grid>
-
-      </Grid>
-      <Pages />
-    </Layout>
-  </React.Fragment>
-    
-
-)
+        <Pages />
+      </Layout>
+    </React.Fragment>
+  )
+}
 
 export default IndexPage
