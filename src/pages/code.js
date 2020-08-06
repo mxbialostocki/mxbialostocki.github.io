f4cf10888d79ac0e19598b4c4887548ed043e3a9
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-07 10:24:45 +1200

    add body to code page

--- src/pages/code.js
+++ src/pages/code.js
@@ -9,11 +9,14 @@ import ProjectScaffolding from '../components/projectScaffolding'
 const Code = () => (
   <Layout>
     <SEO title="code" />
-    <Typography variant="h5">
-      Welcome to some cool code things
-    </Typography>
+    
     <Grid container direction="row" justify="space-between" spacing={10}>
 
+      <Grid item md={10}>
+        <Typography variant="h5">
+          I'm a full-stack dev with a current focus on web development. My preferred resources are below, along with a few projects I've worked on lately. I'm always open to new challenges, so please get in touch with me if you'd like to work together.
+        </Typography>
+      </Grid>
       <Grid item md={6} sm={12}>
         <ProjectWSIR />
       </Grid>
