140293ac755f6549315d5f56c73fdec3563a7495
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-06 12:54:22 +1200

    header links >> activeStyle

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
 import { Grid, Typography } from "@material-ui/core"
+
+import Link from '../components/link'
 
 import useStyle from "../../plugins/custom-mui-theme/theme/custom"
 
@@ -16,17 +17,17 @@ const Header = ({ siteTitle }) => {
           <Grid container direction='row' justify='flex-start' alignItems='flex-start' spacing={2}>
             
               <Grid item lg={3} xs={12}>
-                <Link to="/" activeStyle={{ color: '#f50057' }}>
+                <Link to="/">
                   <Typography variant={"h5"}>{siteTitle}</Typography>
                 </Link>
               </Grid>
               <Grid item lg={3} xs={12}>
-                <Link to="/photography" activeStyle={{ color: '#f50057' }}>
+                <Link to="/photography">
                   <Typography variant={"h5"}>photography</Typography>  
                 </Link>            
               </Grid>
               <Grid item lg={3} xs={12}>
-                <Link to="/code" activeStyle={{ color: '#f50057' }}>
+                <Link to="/code">
                   <Typography variant={"h5"}>code</Typography>  
                 </Link>            
               </Grid>
--- src/components/link.js
+++ src/components/link.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { Link as GatsbyLink } from "gatsby"
 
 // Since DOM elements <a> cannot receive activeClassName and partiallyActive, destructure the prop here and pass it only to GatsbyLink
-const Link = ({ children, to, activeClassName, partiallyActive, ...other }) => {
+const Link = ({ children, to, activeClassName, activeStyle, partiallyActive, ...other }) => {
   // This test assumes that any internal link (intended for Gatsby) will start with exactly one slash, and that anything else is external.
   const internal = /^\/(?!\/)/.test(to)
-
+  
   // Use Gatsby Link for internal links, and <a> for others
   if (internal) {
     return (
       <GatsbyLink
         to={to}
         activeClassName={activeClassName}
+        activeStyle={{ color: '#f50057' }}
         partiallyActive={partiallyActive}
         {...other}
       >
