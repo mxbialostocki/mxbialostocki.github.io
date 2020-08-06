479264118647a67e516fd77d434b5144676e39ba
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-07 10:24:30 +1200

    create contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
+
+import useStyles from '../../plugins/custom-mui-theme/theme/custom'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const ContactPage = () => {
+  const { primary } = useStyles()
+  return (
+    <React.Fragment>
+      <Layout>
+        <SEO title='Contact'/>
+        <Grid container direction="column" spacing={4}>
+          <Grid item xs={12}>
+            <Typography variant="h5">
+              There is a range of ways we can work together. Drop me a note to book dev work or a photography contract, or give me a call on <span className={primary}>+64 0274 658 977</span>.
+            </Typography>
+          </Grid>
+          
+          <Grid item xs={12}>
+            <Typography variant="h5">
+              text
+            </Typography>
+          </Grid>
+        </Grid>
+      </Layout>
+    </React.Fragment>
+  )
+}
+
+export default ContactPage
