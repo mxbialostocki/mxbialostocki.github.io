cd4713ff9d1b6b034b4ad8c42d1418344c05c2fb
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-07 17:02:25 +1200

    contact form sorted

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,55 +1,86 @@
-import React from "react"
-import { Grid, Typography, Button, TextField, FormControl } from "@material-ui/core"
+import React, { useState }  from "react"
+import axios from "axios";
+// import Link from "../components/link"
+import { Grid, Button, FormControl, Input, InputLabel, Typography } from "@material-ui/core"
 
-import useStyles from "../../plugins/custom-mui-theme/theme/custom"
-
-import Link from '../components/link'
-
-const ContactForm = () => {
-
-  const { primary } = useStyles()
-
+const ContactFormTut = () => {
+    
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null
+  });
+  const handleServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg }
+    });
+    if (ok) {
+      form.reset();
+    }
+  };
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    setServerState({ submitting: true });
+    axios({
+      method: "post",
+      url: "https://getform.io/f/e6fbff81-ae54-403c-9dc0-14bbd1571009",
+      data: new FormData(form)
+    })
+      .then(r => {
+        handleServerResponse(true, "Thanks!", form);
+      })
+      .catch(r => {
+        handleServerResponse(false, r.response.data.error, form);
+      });
+  };
   return (
     <React.Fragment>
-      <form method="post" action="https://getform.io/f/e6fbff81-ae54-403c-9dc0-14bbd1571009">
-        <Grid container direction='row' justify='flex-start' alignItems='flex-start' spacing={2}>
-        
-          <Grid item lg={9} xs={12}>
-            <label id="form-user">
-              <FormControl fullWidth>
-                <Typography variant="subtitle1">What's your name?</Typography>
-                <TextField id="form-user" autoComplete defaultValue="" />
-              </FormControl>              
-            </label>
-          </Grid>
-          <Grid item lg={9} xs={12}>
-            <label id="form-email">
-              <FormControl fullWidth>
-                <Typography variant="subtitle1">What's your email address?</Typography>
-                <TextField id="form-email" autoComplete defaultValue="" />
-              </FormControl>              
-            </label>
-          </Grid>
-          <Grid item lg={9} xs={12}>
-            <label id="form-content">
-              <FormControl fullWidth>
-                <Typography variant="subtitle1">Tell me how I can help you:</Typography>
-                <TextField id="form-content" variant="outlined" multiline rows={9} defaultValue="" />
-              </FormControl>              
-            </label>
-          </Grid>
-          <Grid item lg={9} xs={12}>
-            <label id="form-submit">
-              <FormControl fullWidth>
-                <Button type="submit" xs={6}>hit send</Button>
-              </FormControl>              
-            </label>
-          </Grid>
+      <Typography variant="h3">Contact</Typography>
 
-        </Grid>
-      </form>
-    </React.Fragment>
+        <form onSubmit={handleOnSubmit} action="https://getform.io/f/e6fbff81-ae54-403c-9dc0-14bbd1571009">
+          <Grid container direction='row' justify='flex-start' alignItems='flex-start' spacing={2}>
+
+            <Grid item lg={9} xs={12}>            
+              <FormControl fullWidth>
+                <InputLabel htmlFor="name-input">What do you like to be called?</InputLabel>
+                <Input id="form-user" name="name" type="text" required aria-describedby="nameHelp" autoComplete/>
+              </FormControl>              
+            </Grid>
+            <Grid item lg={9} xs={12}>            
+              <FormControl fullWidth>
+                <InputLabel htmlFor="email-input">What's the best email to reach you on?</InputLabel>
+                <Input id="form-email" name="email" type="email" required aria-describedby="emailHelp" autoComplete/>
+              </FormControl>              
+            </Grid>
+            <Grid item lg={9} xs={12}>            
+              <FormControl fullWidth>
+                <InputLabel htmlFor="email-message">Tell me how I can help you:</InputLabel>
+                <Input id="form-message" name="message" type="text" multiline required aria-describedby="theMessage" autoComplete/>
+              </FormControl>              
+            </Grid>
+            <Grid item lg={9} xs={12}>
+              <FormControl fullWidth>
+                <Button>
+                  <Input id="form-submit" type="submit" value="HIT&nbsp;SEND" disableUnderline aria-describedby="submit your message" disabled={serverState.submitting}/>
+                </Button>
+              </FormControl>
+            </Grid>
+            <Grid item lg={9} xs={12}>
+              <Typography variant="h5">
+                {serverState.status && (
+                    <p className={!serverState.status.ok ? "errorMsg" : ""}>
+                    {serverState.status.msg}
+                    </p>
+                )}
+              </Typography>
+            </Grid>
+          </Grid>
+        </form>
+         
+  </React.Fragment>
+     
   )
 }
 
-export default ContactForm
+export default ContactFormTut;
