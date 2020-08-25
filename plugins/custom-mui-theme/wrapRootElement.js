0859fb020f01fff93f1753d9aa4434572911e483
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-25 16:26:20 +1200

    remove console.log from theme provider

--- plugins/custom-mui-theme/wrapRootElement.js
+++ plugins/custom-mui-theme/wrapRootElement.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@material-ui/core/styles"
 import theme from "./theme"
 
 export const wrapRootElement = ({ element }) => {
-  console.info(`theme`, theme)
+  // de-un-comment the following line to spit the theme into the console when building
+  // console.info(`theme`, theme)
   return <ThemeProvider theme={theme}>{element}</ThemeProvider>
 }
